e0a617256599df30e86ce75ef0de2606351248a6
Ahaider60 <alihaiderizvi.you@gmail.com>
2021-07-18 17:50:38 +0500

    Experience Section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,112 @@
+// Import: react DOM
 import React from "react";
+// Imports: Material UI Core
+import Grid from "@material-ui/core/Grid";
+// Imports: Material UI Icons
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 // Imports: Internal Css
 import "./Experience.css";
 const Experience = () => {
-	return <div className='experience'></div>;
+	return (
+		<div className='experience'>
+			<div className='services__heading'>
+				<div className='Services__breadcrumbs'>
+					<p>
+						<a href='/'>Home</a>
+						<KeyboardArrowRightIcon />
+					</p>
+					<p>
+						<a href='/'>AboutUs</a>
+						<KeyboardArrowRightIcon />
+					</p>
+					<p>
+						<a href='/'>Skills</a>
+						<KeyboardArrowRightIcon />
+					</p>
+					<p>
+						<a href='/'>Services</a>
+						<KeyboardArrowRightIcon />
+					</p>
+					<p>
+						Experience
+						<KeyboardArrowRightIcon />
+					</p>
+				</div>
+				<h1>Experience</h1>
+			</div>
+			<Grid
+				container
+				spacing={3}
+				justify='space-evenly'
+				style={{ marginTop: "2rem" }}
+			>
+				<Grid item xs={12} sm={5} className='experience__child'>
+					<div className='experience__icon'>
+						<i class='fas fa-user-graduate'></i>
+					</div>
+					<h4>2014-2015</h4>
+					<h2>Master Degree of Design</h2>
+					<div className='experience__locationIcon'>
+						<i class='fas fa-map-marker-alt'></i>
+						<p> Muhammad Ali Jinnah University</p>
+					</div>
+					<p>
+						A small river named Duden flows by their place and supplies it with
+						the necessary regelialia. It is a paradisematic country, in which
+						roasted parts of sentences fly into your mouth.
+					</p>
+				</Grid>
+				<Grid item xs={12} sm={5} className='experience__child'>
+					<div className='experience__icon'>
+						<i class='fas fa-user-graduate'></i>
+					</div>
+					<h4>2014-2015</h4>
+					<h2>Master Degree of Design</h2>
+					<div className='experience__locationIcon'>
+						<i class='fas fa-map-marker-alt'></i>
+						<p> Muhammad Ali Jinnah University</p>
+					</div>
+					<p>
+						A small river named Duden flows by their place and supplies it with
+						the necessary regelialia. It is a paradisematic country, in which
+						roasted parts of sentences fly into your mouth.
+					</p>
+				</Grid>
+				<Grid item xs={12} sm={5} className='experience__child'>
+					<div className='experience__icon'>
+						<i class='fas fa-user-graduate'></i>
+					</div>
+					<h4>2014-2015</h4>
+					<h2>Master Degree of Design</h2>
+					<div className='experience__locationIcon'>
+						<i class='fas fa-map-marker-alt'></i>
+						<p> Muhammad Ali Jinnah University</p>
+					</div>
+					<p>
+						A small river named Duden flows by their place and supplies it with
+						the necessary regelialia. It is a paradisematic country, in which
+						roasted parts of sentences fly into your mouth.
+					</p>
+				</Grid>
+				<Grid item xs={12} sm={5} className='experience__child'>
+					<div className='experience__icon'>
+						<i class='fas fa-user-graduate'></i>
+					</div>
+					<h4>2014-2015</h4>
+					<h2>Master Degree of Design</h2>
+					<div className='experience__locationIcon'>
+						<i class='fas fa-map-marker-alt'></i>
+						<p> Muhammad Ali Jinnah University</p>
+					</div>
+					<p>
+						A small river named Duden flows by their place and supplies it with
+						the necessary regelialia. It is a paradisematic country, in which
+						roasted parts of sentences fly into your mouth.
+					</p>
+				</Grid>
+			</Grid>
+		</div>
+	);
 };
 
 export default Experience;
